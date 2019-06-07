d42bdae2d08f11cf7970d6c97755d2572b914869
Stephen Whitmore <stephen.whitmore@gmail.com>
2019-06-06 20:43:29 -0700

    it works!

---
+++ index.js
@@ -0,0 +1,59 @@
+const speech = require('@google-cloud/speech');
+const record = require('node-record-lpcm16');
+
+module.exports = function (cb) {
+  const client = new speech.SpeechClient();
+
+  const request = {
+    config: {
+      encoding: 'LINEAR16',
+      sampleRateHertz: 16000,
+      languageCode: 'en-US'
+    },
+    interimResults: true
+  };
+
+  var workingResult
+  var lastResult
+  const recognizeStream = client
+    .streamingRecognize(request)
+    .on('error', cb)
+    .on('data', data => {
+      workingResult = data.results[0].alternatives[0].transcript
+      lastResult = new Date().getTime()
+    });
+
+  var stream = record
+    .start({
+      sampleRateHertz: 16000,
+      threshold: 0,
+      verbose: false,
+      recordProgram: 'arecord',
+      silence: '1.0',
+      device: 'plughw:0,0'
+    })
+
+  stream
+    .on('error', cb)
+    .pipe(recognizeStream);
+
+  stream.once('data', function () {
+    console.log('listening..')
+  })
+
+  var ix = setInterval(function () {
+    var now = new Date().getTime()
+    if (workingResult && now - lastResult > 1000) {
+      finish(null, workingResult)
+    } else if (!workingResult && now - lastResult > 3000) {
+      finish()
+    }
+  }, 500)
+
+  function finish (err, res) {
+    record.stop()
+    stream.end()
+    clearInterval(ix)
+    cb(err, workingResult)
+  }
+}
